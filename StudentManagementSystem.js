f45dd6e31a7d9d77846f62215bce2cead5bf646d
EmnaNaili <72765098+EmnaNaili@users.noreply.github.com>
2021-04-06 12:47:43 +0100

    first

--- StudentManagementSystem.js
+++ StudentManagementSystem.js
@@ -2,3 +2,18 @@ function add (number1, number2){
     return number1 + number2
 };
 add(1,2)
+var NumberOfStudents=0;
+function addStudent(name) {
+   NumberOfStudents ++ ;
+   }
+   console.log(NumberOfStudents) 
+   addStudent()
+   console.log(NumberOfStudents) 
+   addStudent()
+   console.log(NumberOfStudents) 
+
+
+ function getNumberOfStudents() {
+
+ return NumberOfStudents 
+   }
